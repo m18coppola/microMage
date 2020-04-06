f7358d04e2b5acb3c5382d5d6c214f1a0ddc0f81
Michael Coppola <m18coppola@gmail.com>
2020-04-06 15:53:32 -0400

    init commit

---
+++ desktop/src/com/final/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.final.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.final.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
