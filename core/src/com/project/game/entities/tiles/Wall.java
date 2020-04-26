2c5ba7519ab4f73b1df42a5ba8255504e08431b9
Michael Coppola <m18coppola@gmail.com>
2020-04-26 00:13:13 -0400

    started wall implementation.

--- core/src/com/project/game/entities/tiles/Wall.java
+++ core/src/com/project/game/entities/tiles/Wall.java
@@ -1,4 +1,17 @@
 package com.project.game.entities.tiles;
 
-public class Wall {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Wall extends Tile {
+    enum WallType {
+
+    }
+    public Wall(float x, float y, Sprite sprite) {
+        super(x, y, sprite);
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
 }
