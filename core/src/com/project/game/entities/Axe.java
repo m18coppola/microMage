6319dc9f4c50e7be213f582400acf62cfe115a31
ShahHassan12 <s22syed@siena.edu>
2020-04-28 01:28:56 -0400

    Added axe, the weapon for barbarian

---
+++ core/src/com/project/game/entities/Axe.java
@@ -0,0 +1,32 @@
+package com.project.game.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.project.game.ResourceLoader;
+
+public class Axe extends Entity{
+    Vector2 velocity;
+    Sprite sprite;
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 9;
+    public static final int SPEED = 100;
+
+    public Axe(double angle, Vector2 position){
+        super(position.x - WIDTH/2, position.y - HEIGHT / 2, WIDTH, HEIGHT);
+        sprite = ResourceLoader.loadFireBall();
+        velocity = new Vector2((float)(Math.cos(angle)*SPEED), (float)(Math.sin(angle)*SPEED));
+    }
+    public Sprite getSprite(){
+        return sprite;
+    }
+
+    public void update(float dt){
+        hitbox.setPosition(hitbox.getX() + velocity.x * dt,
+                hitbox.getY() + velocity.y * dt);
+    }
+
+    public void dispose(){
+        sprite.getTexture().dispose();
+    }
+
+}
