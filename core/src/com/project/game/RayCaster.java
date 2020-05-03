0429da7a3967003bd14eb5caca25e27bad5e5862
Michael Coppola <m18coppola@gmail.com>
2020-05-03 19:02:08 -0400

    started creating raymarcher

--- core/src/com/project/game/RayCaster.java
+++ core/src/com/project/game/RayCaster.java
@@ -1,4 +1,39 @@
 package com.project.game;
 
+import com.badlogic.gdx.math.Vector2;
+import com.project.game.entities.tiles.Wall;
+
 public class RayCaster {
+    Vector2 position;
+    Wall[] walls;
+    public RayCaster(float x, float y, Wall[] walls){
+        position.set(x, y);
+        this.walls = walls;
+    }
+    class Ray extends Thread{
+        static final float MARCH_SIZE = 1f;
+        private float angle;
+        Wall[] walls;
+        Vector2 position;
+        boolean found = false;
+
+        public Ray(float x, float y, float angle, Wall[] walls){
+            this.angle = angle;
+            position.set(x, y);
+            this.walls = walls;
+        }
+
+        @Override
+        public void run(){
+            while(!found){
+                for(Wall w:walls){
+                    if(w.hitbox.contains(position)){
+                        found = true;
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
 }
